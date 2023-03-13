99f7625bbd0a9a4a3c5902294f275f55fd60403a
dmitrii sh <dmitrii@sh.com>
2023-03-13 22:46:07 +0300

    3. Настройка проекта

---
+++ scripts/script_3.sql
@@ -0,0 +1,8 @@
+create table users
+(
+    username varchar(128) primary key,
+    firstname varchar(128),
+    lastname varchar(128),
+    birth_date date,
+    age int
+);
---
+++ src/test/java/ru/edu/AppTest.java
@@ -0,0 +1,14 @@
+package ru.edu;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
