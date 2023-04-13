207cea61969485e9f538c67bc485e7f46e74388d
dmitrii sh <dmitrii@sh.com>
2023-04-13 22:12:46 +0300

    9. Transactions & Locks 6. Read Only Transactions

--- src/main/java/ru/edu/HibernateRunner.java
+++ src/main/java/ru/edu/HibernateRunner.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.annotations.QueryHints;
 import org.hibernate.graph.GraphSemantic;
 import org.hibernate.graph.RootGraph;
 import org.hibernate.graph.SubGraph;
@@ -24,31 +25,31 @@ public class HibernateRunner {
 
   public static void main(String[] args) {
     try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-      Session session = sessionFactory.openSession();
-      Session session1 = sessionFactory.openSession()) {
+      Session session = sessionFactory.openSession()) {
+
+      //ReadOnly для всего
+//      session.setDefaultReadOnly(true);
+
       session.beginTransaction();
-      session1.beginTransaction();
 
-      // PESSIMISTIC_FORCE_INCREMENT 0 плохая практика.
-      // Для списка будет сделан запрос на каждую запись, чтобы обновить версионность
-      session.createQuery("select p from Payment p", Payment.class)
-        .setLockMode(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
-        // timeout 5 сек на блокировку на запрос, потом блокировка будет снята
-        .setHint("javax.persistence.lock.timeout", 5000)
-        .list();
+      // бросит ошибку при измении бд
+      session.createNativeQuery("SET TRANSACTION READ ONLY").executeUpdate();
 
-      // в двух сессиях берем одну запись и пытаемся ее изменить
-      var payment = session.find(Payment.class, 1L, LockModeType.PESSIMISTIC_FORCE_INCREMENT);
+      // ReadOnly для запроса
+//      session.createQuery("select p from Payment p", Payment.class)
+//        .setLockMode(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
+//        .setReadOnly(true)
+//        // ИЛИ
+////        .setHint(QueryHints.READ_ONLY, true)
+//        .list();
+
+      var payment = session.find(Payment.class, 1L);
+      // ReadOnly для объекта payment
+//      session.setReadOnly(payment, true);
+      // не сделает изменения при завершении транзакции, т.к. ReadOnly выше
       payment.setAmount(payment.getAmount() + 10);
 
-      var theSamePayment = session1.find(Payment.class, 1L);
-      theSamePayment.setAmount(theSamePayment.getAmount() + 20);
 
-      // session1 зависнет, т.к. не может сделать коммит
-      // посколько session заблокировала доступ через PESSIMISTIC_READ - for share
-      // PESSIMISTIC_WRITE - for update
-      // PESSIMISTIC_FORCE_INCREMENT - for update nowait
-      session1.getTransaction().commit();
       session.getTransaction().commit();
     }
   }
